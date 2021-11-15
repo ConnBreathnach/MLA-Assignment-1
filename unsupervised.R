231e85cac6a58a5e38be4122d0d57371e18bbae8
Conn Breathnach <cobreath@tcd.ie>
2021-11-15 08:09:58 +0000

    Some obs done

--- unsupervised.R
+++ unsupervised.R
@@ -1,1 +1,19 @@
 data <- read.csv("data/hfd.csv")
+unlabelled_data <- data[, 1:4]
+summary(unlabelled_data)
+
+
+pcs <- prcomp(unlabelled_data)
+print(pcs)
+summary(pcs)
+round(pcs$rotation, 2)
+
+pc_mat <- as.matrix(pcs[["x"]])
+pc_dist <- dist(pc_mat)
+pc_hier_cluster <- hclust(pc_dist, method="single")
+plot(pc_hier_cluster)
+
+bloods_dist <- dist(unlabelled_data)
+bloods__hier_cluster <- hclust(bloods_dist, method = "average")
+
+plot(bloods__hier_cluster)
